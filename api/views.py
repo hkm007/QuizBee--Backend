5f1fd641763bbc7c774714695b7bd90bb3608d86
hkm007 <hkm0072001@gmail.com>
2020-07-09 12:41:45 +0530

    user app created and separated

--- api/views.py
+++ api/views.py
@@ -1,16 +1,6 @@
-from django.contrib.auth.models import User
 from .models import Quiz
 from rest_framework import viewsets
-from .serializers import UserSerializer, QuizSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-
+from .serializers import QuizSerializer
 
 class QuizViewSet(viewsets.ModelViewSet):
     """
--- server/settings.py
+++ server/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'api',
+    'user',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
